097c543e1f76fd03f4887ab40f307fd755adc035
ARGAMERX1 <67414555+ARGAMERX1@users.noreply.github.com>
2020-07-15 14:21:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var obsatacleGroup;
+var shooter;
+var bullet;
+var line1;
+var line2;
+var line3;
+var line4;
+var line5;
+var line6;
+var line7;
+var line8;
+var line9;
+var line10;
+var line11;
+var line12;
+var line13;
+var line14;
+var line15;
+var line16;
+var line17;
+var line18;
+var line19;
+var line20;
+var line21;
+var line22;
+var line23;
+var line24;
+var line25;
+var line26;
+var line27;
+var line28;
+var line29;
+var line30;
+var line31;
+var line32;
+var line33;
+var line34;
+var line35;
+var line36;
+var line37;
+var line38;
+var line39;
+var line40;
+var line41;
+var line42;
+var line43;
+var line44;
+function setup() {
+  createCanvas(800, 800);
+
+var line1 = createSprite(3,780,3,100);
+
+}
+
+function draw() {
+  background("red");
+
+
+
+  drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
